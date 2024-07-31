76a5c535a7cc32cfbdc43ec011fc5c31c6183345
yksl <yukselkurtuldu9@gmail.com>
2024-07-31 17:13:10 +0300

    up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -34,7 +34,7 @@ const user = {
         })
     },
     update: async (req, res) => {
-        const data = await User.updateOne({_id: req.params.id}, req.body)
+        const data = await User.updateOne({_id: req.params.id}, req.body, {runValidators: true})
 
         res.status(202).send({
             error: false,
