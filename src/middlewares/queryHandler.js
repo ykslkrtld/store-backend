3368bd5daa4653e765522d25e4f7e7882146e5a7
yksl <yukselkurtuldu9@gmail.com>
2024-08-02 10:41:29 +0300

    up

--- src/middlewares/queryHandler.js
+++ src/middlewares/queryHandler.js
@@ -5,28 +5,24 @@
 // app.use(findSearchSortPage):
 
 module.exports = (req, res, next) => {
-  // Searching & Sorting & Pagination:
 
   const filter = req.query?.filter || {};
 
-  // SEARCHING: URL?search[key1]=value1&search[key2]=value2
   const search = req.query?.search || {};
   for (let key in search) search[key] = { $regex: search[key], $options: "i" };
 
-  // Cancelled -> SORTING: URL?sort[key1]=1&sort[key2]=-1 (1:ASC, -1:DESC)
-  // mongoose=^8.0 -> SORTING: URL?sort[key1]=asc&sort[key2]=desc (asc: A->Z - desc: Z->A)
   const sort = req.query?.sort || {};
 
   // PAGINATION: URL?page=1&limit=10
   // LIMIT:
-  let limit = Number(req.query?.limit);
-  limit = limit > 0 ? limit : Number(process.env?.PAGE_SIZE || 20);
-  // PAGE:
-  let page = Number(req.query?.page);
-  page = (page > 0 ? page : 1) - 1;
-  // SKIP:
-  let skip = Number(req.query?.skip);
-  skip = skip > 0 ? skip : page * limit;
+  let limit = Number(req.query?.limit)
+    limit = limit > 0 ? limit : Number(process.env?.PAGE_SIZE || 20)
+    // Page
+    let page = Number(req.query?.page)
+    page = page > 0 ? page : 1
+    // Skip
+    let skip =Number(req.query?.skip)
+    skip = skip > 0 ? skip : ((page-1) * limit)
 
   // Run SearchingSortingPagination engine for Model:
   res.getModelList = async function (Model, customFilters = {}, populate = null) {
@@ -49,16 +45,15 @@ module.exports = (req, res, next) => {
       limit,
       page,
       pages: {
-        previous: page > 0 ? page : false,
-        current: page + 1,
-        next: page + 2,
-        total: Math.ceil(data.length / limit),
+        previous: (page > 1 ? page-1 : false),
+        current: page,
+        next: page + 1,
+        total: Math.ceil(data.length / limit)
       },
       totalRecords: data.length,
     };
-    details.pages.next =
-      details.pages.next > details.pages.total ? false : details.pages.next;
-    if (details.totalRecords <= limit) details.pages = false;
+    if(details.pages.next > details.pages.total) details.pages.next = false
+        if (details.totalRecords <= limit) details.pages = false
     return details;
   };
 
